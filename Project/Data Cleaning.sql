793ac2276a68243f245748399f002a04f00a5bba
Rushikesh Patil <rushikesh1502@gmail.com>
2025-10-22 19:53:40 +0530

    Removed Duplicates

--- Project/Data Cleaning.sql
+++ Project/Data Cleaning.sql
@@ -7,9 +7,54 @@
 create table layoffs_staging
 like layoffs;
 
-select *
-from layoffs_staging;
-
 insert into layoffs_staging
 select *
 from layoffs;
+
+with dup_cte as
+(select * ,
+row_number() over(partition by 
+company , location , industry , total_laid_off , percentage_laid_off , `date` , stage,country , funds_raised_millions) as row_num
+from layoffs_staging
+)
+select *
+from dup_cte
+where row_num > 1
+;
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert into layoffs_staging2
+select * ,
+row_number() over(partition by 
+company , location , industry , total_laid_off , percentage_laid_off , `date` , stage,country , funds_raised_millions) as row_num
+from layoffs_staging;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM layoffs_staging2
+WHERE row_num > 1;
+
+SET SQL_SAFE_UPDATES = 1;
+
+select *
+from layoffs_staging2;
+
+
+
+
+
+
+
